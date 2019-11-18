84e61f6ca19c01058f28ee2b6a046c1be005e122
kchangch <kchangch@sfu.ca>
2019-11-17 17:02:26 -0800

    create dataframes for each genre to analyze it later

--- movie-data/dataplot.py
+++ movie-data/dataplot.py
@@ -24,11 +24,37 @@ def plot_fig(x_axis, y_axis, title, xlabel, ylabel):
     x_axis = ax1.axes.get_xaxis()
     x_axis.set_visible(False)
 
-    
+def getGenreAndRating(data, genreColumn, genreRating, title, rating, genre):
+    temp = pd.DataFrame()
+    temp[genreColumn] = data[title][data[genre].str.contains(genreColumn)]
+    temp[genreRating] = data[rating][data[genre].str.contains(genreColumn)]
+    return temp
 
-plot_fig(title, rating, 'Title vs. Rating', 'Title', 'Rating')
-plot_fig(director, rating, 'Director vs. Rating', 'Director', 'Rating')
-plot_fig(runtime, rating, 'Runtime vs. Rating', 'Runtime', 'Rating')
+action = pd.DataFrame()
+adventure = pd.DataFrame()
+comedy = pd.DataFrame()
+crime = pd.DataFrame()
+scifi = pd.DataFrame()
+drama = pd.DataFrame()
+horror = pd.DataFrame()
+romance = pd.DataFrame()
+mystery = pd.DataFrame()
 
-plt.show()
+action[['Action', 'ActionRating']] = getGenreAndRating(data, 'Action', 'ActionRating', 'title', 'rating', 'genre')
+adventure[['Adventure', 'AdventureRating']] = getGenreAndRating(data, 'Adventure', 'AdventureRating', 'title', 'rating', 'genre')
+comedy[['Comedy', 'ComedyRating']] = getGenreAndRating(data, 'Comedy', 'ComedyRating', 'title', 'rating', 'genre')
+crime[['Crime', 'CrimeRating']] = getGenreAndRating(data, 'Crime', 'CrimeRating', 'title', 'rating', 'genre')
+scifi[['Sci-Fi', 'SciFiRating']] = getGenreAndRating(data, 'Sci-Fi', 'SciFiRating', 'title', 'rating', 'genre')
+drama[['Drama', 'DramaRating']] = getGenreAndRating(data, 'Drama', 'DramaRating', 'title', 'rating', 'genre')
+horror[['Horror', 'HorrorRating']] = getGenreAndRating(data, 'Horror', 'HorrorRating', 'title', 'rating', 'genre')
+romance[['Romance', 'RomanceRating']] = getGenreAndRating(data, 'Romance', 'RomanceRating', 'title', 'rating', 'genre')
+
+
+
+
+# plot_fig(title, rating, 'Title vs. Rating', 'Title', 'Rating')
+# plot_fig(director, rating, 'Director vs. Rating', 'Director', 'Rating')
+# plot_fig(runtime, rating, 'Runtime vs. Rating', 'Runtime', 'Rating')
+
+# plt.show()
 
