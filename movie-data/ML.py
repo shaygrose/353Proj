bf398de9378d4a7cde21e9440f253766eb433086
Shayna Grose <sgrose@sfu.ca>
2019-11-25 20:32:01 -0800

    added rand forest classifier in ML.py

--- movie-data/ML.py
+++ movie-data/ML.py
@@ -6,10 +6,11 @@ from skimage.color import lab2rgb
 from skimage.color import rgb2lab
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.preprocessing import FunctionTransformer
+from sklearn.preprocessing import FunctionTransformer, PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
 
 
 data = pd.read_csv(sys.argv[1], index_col = 0)
@@ -31,8 +32,12 @@ data['actor2_code'] = data['actor2'].apply(get_director_index, leest = actor_2_n
 data['actor3_code'] = data['actor3'].apply(get_director_index, leest = actor_3_name)
 data['genre_code'] = data['genre'].apply(get_director_index, leest = genre_genre)
 
-print(data.head())
-data.to_csv('text2.csv')
+#converting runtime to hours and rounding to nearest decimal place
+data['runtime'] = (data['runtime']/60).round(1)
+# print(data['runtime'].head())
+
+#print(data.head())
+#data.to_csv('text2.csv')
 
 def score_polyfit(n):
     model = make_pipeline(
@@ -40,4 +45,37 @@ def score_polyfit(n):
         LinearRegression(fit_intercept = False)
     )
     model.fit(X_train, y_train)
-    print(n, model.score(X_valid, y_valid))
+    print('n=%i: score=%.5g' % (n, model.score(X_valid, y_valid)))
+
+X = data[['director code', 'runtime', 'genre_code']]
+#round each rating to the nearest whole number
+y = data['rating'].round(0)
+X_train, X_valid, y_train, y_valid = train_test_split(X,y,test_size = 0.33, random_state=42)
+
+rand_forest_model = make_pipeline(
+        RandomForestClassifier(n_estimators=100,
+        max_depth=10, min_samples_leaf=16)
+    )
+
+#throws an error
+rand_forest_model.fit(X_train, y_train)
+print(rand_forest_model.score(X_valid, y_valid))
+
+#score for just director and genre
+#0.389819587628866
+
+#score for director, runtime and genre
+#0.40850515463917525
+
+# score_polyfit(1)
+# score_polyfit(5)
+# score_polyfit(9)
+# score_polyfit(13)
+# score_polyfit(17)
+
+# for just director and genre
+# n=1: score=0.0039917
+# n=5: score=-1.0558
+# n=9: score=-2830.2
+# n=13: score=-5995.4
+# n=17: score=-27.439
