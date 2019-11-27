a6ed9707321775faff43974f6cca8b3142ac5c57
pna18 <pna18@sfu.ca>
2019-11-26 22:50:16 -0800

    printed out all the stat tests

--- movie-data/ML.py
+++ movie-data/ML.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+import random
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import FunctionTransformer
@@ -18,16 +19,24 @@ from sklearn.preprocessing import StandardScaler
 data = pd.read_csv(sys.argv[1], index_col = 0)
 data.drop(columns = ['index'])
 
+def random_genre(x):
+    return random.choice(x.split(','))
+
+data['genre_one'] = data['genre'].apply(random_genre)
+
 directors_names = data['director'].unique()
 actor_1_name = data['actor1'].unique()
 actor_2_name = data['actor2'].unique()
 actor_3_name = data['actor3'].unique()
 genre_genre = data['genre'].unique()
 decade_code = data['decade'].unique()
+genre_codecode = data['genre_one'].unique()
 
 def get_director_index(x, leest):
     #print(np.where(directors_names == x))
     return np.where(leest == x)[0][0]
+
+
 
 data['director code'] = data['director'].apply(get_director_index, leest = directors_names)
 data['actor1_code'] = data['actor1'].apply(get_director_index, leest = actor_1_name)
@@ -35,6 +44,8 @@ data['actor2_code'] = data['actor2'].apply(get_director_index, leest = actor_2_n
 data['actor3_code'] = data['actor3'].apply(get_director_index, leest = actor_3_name)
 data['genre_code'] = data['genre'].apply(get_director_index, leest = genre_genre)
 data['decade_code'] = data['decade'].apply(get_director_index, leest = decade_code)
+data['genre_one_code'] = data['genre_one'].apply(get_director_index, leest = genre_codecode)
+
 
 #converting runtime to hours and rounding to nearest decimal place
 data['runtime'] = (data['runtime']/60).round(1)
@@ -52,9 +63,11 @@ def score_polyfit(n):
     print('n=%i: score=%.5g' % (n, model.score(X_valid, y_valid)))
 
 X = data[['director code', 'runtime', 'genre_code', 'decade_code']]
+X_2 = data[['director code', 'runtime','genre_one_code', 'decade_code']]
 #round each rating to the nearest whole number
 y = data['rating'].round(0)
 X_train, X_valid, y_train, y_valid = train_test_split(X,y,test_size = 0.33, random_state=42)
+X_train_2, X_valid_2, y_train_2, y_valid_2 = train_test_split(X_2,y,test_size = 0.33, random_state=42)
 
 rand_forest_model = make_pipeline(
         RandomForestClassifier(n_estimators=100,
@@ -84,6 +97,21 @@ print(voting_model.score(X_valid, y_valid)) # 0.3943298969072165
 
 Boosting_model.fit(X_train, y_train)
 print(Boosting_model.score(X_valid, y_valid)) # 0.4220360824742268
+
+print('  ')
+print('with random_genre')
+print('   ')
+
+rand_forest_model.fit(X_train_2, y_train_2)
+print(rand_forest_model.score(X_valid_2, y_valid_2)) # 0.41365979381443296
+
+voting_model.fit(X_train_2, y_train_2)
+print(voting_model.score(X_valid_2, y_valid_2)) # 0.4117268041237113
+
+Boosting_model.fit(X_train_2, y_train_2)
+print(Boosting_model.score(X_valid_2, y_valid_2)) # 0.41301546391752575
+
+
 
 #score for just director and genre
 #0.389819587628866
